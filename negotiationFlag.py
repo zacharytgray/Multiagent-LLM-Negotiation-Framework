747a8bcc37c6a058b1965b601ce005614da2d7eb
zacharytgray <96278173+zacharytgray@users.noreply.github.com>
2025-01-31 09:29:05 -0600

    Bug fixes

--- negotiationFlag.py
+++ negotiationFlag.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class NegotiationFlag(Enum):
     PROPOSAL_NOT_FOUND = "Proposal not found in response"
-    NOT_ENOUGH_TASKS = "Not enough items in proposal"
-    TOO_MANY_TASKS = "Too many items in proposal"
-    INVALID_TASKS_PRESENT = "Invalid items present in proposal"
+    NOT_ENOUGH_TASKS = "Not enough tasks in proposal"
+    TOO_MANY_TASKS = "Too many tasks in proposal"
+    INVALID_TASKS_PRESENT = "Invalid tasks present in proposal"
     ERROR_FREE = "No errors found"
